80d4589c4b08391d3ede1189e4ea9d5c9a2d4263
mistful <willthedragonslayer@protonmail.com>
2022-06-11 17:25:15 +0200

    Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let splitUpArray = [];
+  splitUpArray = string.split("");
+  splitUpArray.reverse();
+  let result = splitUpArray.join("");
+  return result;
 };
 
 // Do not edit below this line
