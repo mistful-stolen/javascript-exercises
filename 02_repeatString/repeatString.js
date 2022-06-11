5070aa42b66876d7f05a497eb8ed9d2fa9adf1da
mistful <willthedragonslayer@protonmail.com>
2022-06-11 17:14:50 +0200

    Solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let hey = "";
+  if (num < 0) {
+    return "ERROR";
+  }
 
+  for (let i = 1; i <= num; i++) {
+    hey += string;
+  }
+  return hey;
 };
 
 // Do not edit below this line
