ba19e53bd28ee4b45aaabdb0cbcec9cd17ef8d84
mistful <willthedragonslayer@protonmail.com>
2022-06-11 18:11:14 +0200

    Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (...numbers) {
+  for (let number of numbers) {
+    if (number < 0 || typeof number !== "number") {
+      return "ERROR";
+    }
+  }
 
+  let smallestNumber = Math.min(...numbers);
+  let largestNumber = Math.max(...numbers);
+  let sum = 0; // 1, 4 = 1 + 2 + 3 + 4
+  for (let i = smallestNumber; i < smallestNumber + largestNumber; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
