bfd05f659fbb633f60e2a7d7450b0d06f3e4dfe1
mistful <willthedragonslayer@protonmail.com>
2022-06-11 17:46:30 +0200

    Successful 2nd

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (givenArray, ...numbers) {
+  let array = givenArray;
 
+  for (let number of numbers) {
+    let indexOfNumber = array.indexOf(number);
+    array.splice(indexOfNumber, 1);
+  }
+  return array;
 };
 
 // Do not edit below this line
