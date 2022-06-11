bf821b06da826c83ab6b0533b31f39881144cd5b
mistful <willthedragonslayer@protonmail.com>
2022-06-11 18:48:54 +0200

    Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  //[°C] = ([°F] − 32) × 5⁄9
+  let result = ((fahrenheit - 32) * 5) / 9;
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  //[°F] = [°C] × 9⁄5 + 32
+  let result = (celsius * 9) / 5 + 32;
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
